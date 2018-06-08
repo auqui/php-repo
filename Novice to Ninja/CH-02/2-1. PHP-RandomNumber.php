74454f5dee7211fc7d835925be53705c855f74dd
manuel <manuel@ieholding.com>
2018-06-08 15:47:38 -0500

    First PHP file

---
+++ Novice to Ninja/CH-02/2-1. PHP-RandomNumber.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Random Number</title>
+</head>
+<body>
+    <p>Generating a random number between 1 abnd 10</p>
+        <?php
+            echo rand(1, 10);
+        ?>
+</body>
+</html>
